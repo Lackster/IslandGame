3255f7c2a380770aa0fe5191dd6d98935a676a36
Billington B. Bucketswerth <proulxj1@outlook.com>
2015-04-28 12:14:08 -0400

    Commit
    Commit

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -4,30 +4,48 @@ using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
 
+	Rigidbody playerRigidbody;
+
 	IDictionary[] inventoryPlayer;
 	Ray cameraRay;
 	GameObject mainCharacter;
 	int objectID;
 	int inventorySlot;
 	bool inventoryChange;
+	bool invOn;
+	GameObject selectedObject;
+
+	Vector3 objectHitLocation;
+
+	// Actual Objects Under Here. To Assign, make sure they are public!:
+	public GameObject Wood;
+	public GameObject Fish; // I think we are getting Feesh. Lol.
 
 
 	void Start () {
 		inventoryPlayer = new IDictionary[10];
 		cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		mainCharacter = GameObject.FindGameObjectWithTag ("Player");
+		playerRigidbody = new Rigidbody(GetComponent <Rigidbody>());
+
 	}
 	
 	// Update is called once per frame
 
 
+	void Drop()
+	{
+		Quaternion filler = new Quaternion (1, 1, 1, 1);
+		GameObject.Instantiate (Wood, objectHitLocation,filler);
+		objectHitLocation = new Vector3(0,0,0);
 
+	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.tag == "AcceptedObject") 
 		{
-			bool check;
+			bool check = true;
 			switch(collision.gameObject.tag)
 			{
 			case ("Arrow"):
@@ -111,7 +129,7 @@ public class FirstPersonController : MonoBehaviour {
 				{
 					if(inventoryPlayer[inventorySlot].Keys == "tree")
 					{
-						inventoryPlayer[inventorySlot].Add("tree",value:+1;
+						inventoryPlayer[inventorySlot].Add("tree",value:+1);
 						break;
 					}else if (inventoryPlayer.Length == inventorySlot)
 					{
@@ -132,8 +150,65 @@ public class FirstPersonController : MonoBehaviour {
 
 	void Update () 
 	{
+		ContactPoint point;
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			playerRigidbody.AddForce(Vector3.forward * 5, ForceMode.Acceleration); // Difficult to limit this.
+		}
+
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			playerRigidbody.AddForce (Vector3.left * 5, ForceMode.Acceleration);
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			playerRigidbody.AddForce (Vector3.right * 5, ForceMode.Acceleration);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			playerRigidbody.AddForce (Vector3.back * 5, ForceMode.Acceleration);
+		}
+
+		if (Input.GetKeyDown (KeyCode.I)) 
+		{
+			if (invOn)
+			{
+				for (int i = 0; i< inventoryPlayer.Length; i ++)
+				{
+					int guiSlot =1;
+				while(true)
+					{
+						guiSlot = guiSlot+1;
+						GUIContent generatedContent = new GUIContent(inventoryPlayer[guiSlot]);// LOTS OF WORK NEEDED HERE! How do I get the object attatched to the . . You know?
+						
+
+					}
+				}
+			}
+			else
+			{
+
+			}
+
+
+		}
+
+
+		/*Transform playerRot = GetComponent<Transform>();
+		Vector3 mousePosition = Input.mousePosition;
+		if (Input.mousePosition = ! mousePosition) 
+		{
+			playerRot.localRotation = new Quaternion(playerRot.rotation.x + mousePosition.position.x,playerRot.localRotation.y + mousePosition.position.y);
+			mousePosition.position = new Vector3(0,0,0);
+		}
+		*///HEY YO! Me! I need to make sure that this works correctly, so the character can actually look around. k?? k..
+
+
 		Collision hit;
-		if (Physics.Raycast(cameraRay,out hit))
+		if (Physics.Raycast(cameraRay,out hit))//God knows why this is overloading match. It should be fine, but it likes breaking on me. Mess around with it a li'l.
 		{
 			GameObject currView = hit.gameObject;
 
@@ -141,9 +216,9 @@ public class FirstPersonController : MonoBehaviour {
 			{
 				GUI.enabled = true;
 				Rect rect = new Rect(0,0,5,5);
-
+				string fillerz = currView.name;
 				//Made to use different behieviors for different objects.
-				switch(currView.name)
+				switch(fillerz)
 				{
 
 				case("something"):
@@ -183,7 +258,16 @@ public class FirstPersonController : MonoBehaviour {
 					int state;
 				while (inProgress)
 					{
+						if (Input.GetMouseButtonUp(1))
+						{
+							inProgress = false; // Oops. Looks like I didn't need a bool. Might as well use it anyways.
+							break;
+						}
 						if (objectID != objectIDClone)
+						{
+							break;
+						}
+						if (selectedObject.name != "Axe")
 						{
 							break;
 						}
@@ -192,22 +276,24 @@ public class FirstPersonController : MonoBehaviour {
 						if (timeRecorded == timeRequired)
 						{
 							timeRecorded = 0;
+							objectHitLocation = new Vector3(hit.transform.position.x, hit.transform.position.y,hit.transform.position.z);
+							Destroy(hit.gameObject);
+							Drop();
+							break;
+
+
 							// For state movement: state = state++;
 							//bar.size for scrollbar.
 							//increase the size of bar, or other functions.
-
 						}
 						//if State == 5 inprogress{break;}
-
-
-
 
 					}
 
 				break;
 
 			case(1)://ObjectDesc
-
+			
 				break;
 
 			case(2)://ObjectDesc
